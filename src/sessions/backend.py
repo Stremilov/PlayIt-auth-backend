773eae3bb058879fd2cdb11c38b3604e804fdca3
unknown <maks-kandikov@yandex.ru>
2024-12-24 01:32:30 +0300

    fastapi-sessions logic here

---
+++ src/sessions/backend.py
@@ -0,0 +1,19 @@
+from uuid import UUID, uuid4
+from fastapi_sessions.backends.implementations import InMemoryBackend
+from fastapi_sessions.frontends.implementations import SessionCookie, CookieParameters
+
+from src.schemas.sessions import SessionData
+
+cookie_params = CookieParameters(
+    max_age=10
+)
+
+cookie = SessionCookie(
+    cookie_name="cookie",
+    identifier="general_verifier",
+    auto_error=True,
+    secret_key="DONOTUSE",
+    cookie_params=cookie_params,
+)
+
+backend = InMemoryBackend[UUID, SessionData]()
