96fe8ec0e3af811de39ea923ff64a2f09a3a1c01
unknown <maks-kandikov@yandex.ru>
2024-12-24 01:34:25 +0300

    new sessions_router

--- src/api/routers.py
+++ src/api/routers.py
@@ -1,5 +1,7 @@
 from src.api.users import router as users_router
+from src.api.sessions import router as sessions_router
 
 all_routers = [
-    users_router
+    users_router,
+    sessions_router
 ]
