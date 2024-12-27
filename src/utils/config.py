05fa01558ed502024fa84d89a9662cac42098eae
unknown <maks-kandikov@yandex.ru>
2024-12-27 23:41:37 +0300

    добавил SECRET_KEY

--- src/utils/config.py
+++ src/utils/config.py
@@ -3,8 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Для создания БД
 DB_PASSWORD = os.getenv("DATABASE_PASSWORD")
 DB_HOST = os.getenv("DATABASE_HOST", "localhost")
 DB_NAME = os.getenv("DATABASE_NAME", "postgres")
 DB_USER = os.getenv("DATABASE_USER", "postgres")
 DB_PORT = os.getenv("DATABASE_PORT", "5432")
+
+SECRET_KEY = os.getenv("SECRET_KEY")  # Для fastapi-сессий в файле src/sessions/backend
