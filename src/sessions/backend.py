90c606e954062e93db7e37a24dcfedb3657f894f
unknown <maks-kandikov@yandex.ru>
2024-12-27 23:42:03 +0300

    вынес secret_key в .env

--- src/sessions/backend.py
+++ src/sessions/backend.py
@@ -3,16 +3,17 @@ from fastapi_sessions.backends.implementations import InMemoryBackend
 from fastapi_sessions.frontends.implementations import SessionCookie, CookieParameters
 
 from src.schemas.sessions import SessionData
+from src.utils.config import SECRET_KEY
 
 cookie_params = CookieParameters(
-    max_age=60 * 60 * 24 # время действия токена доступа 1 день
+    max_age=60 * 60 * 24  # время действия токена доступа 1 день
 )
 
 cookie = SessionCookie(
     cookie_name="cookie",
     identifier="general_verifier",
     auto_error=True,
-    secret_key="DONOTUSE", #TODO убрать в .env
+    secret_key=SECRET_KEY,
     cookie_params=cookie_params,
 )
 
