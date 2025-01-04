6cc46d49d4eb7acc0b7cbd7743088503be80f5d7
unknown <maks-kandikov@yandex.ru>
2025-01-04 20:56:00 +0300

    реализованы два эндоинта: telegram-login и whoami

--- src/api/routers.py
+++ src/api/routers.py
@@ -1,7 +1,5 @@
 from src.api.users import router as users_router
-from src.api.sessions import router as sessions_router
 
 all_routers = [
-    users_router,
-    sessions_router
+    users_router
 ]
