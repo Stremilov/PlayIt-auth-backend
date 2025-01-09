5289e290246fb8d69cb101e9120ac1c54b0d53c4
Lef <116380867+Stremilov@users.noreply.github.com>
2025-01-10 00:47:18 +0300

    Откат

--- main.py
+++ main.py
@@ -8,21 +8,13 @@ from logging_loki import LokiHandler
 from src.db.db import init_db
 from src.api.routers import all_routers
 
-LOKI_URL = "http://loki:3100/loki/api/v1/push"
-LOKI_TAGS = {"application": "playit-auth"}
-
-loki_handler = LokiHandler(
-    url=LOKI_URL,
-    tags=LOKI_TAGS,
-    version="1",
-)
 
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
-logger = logging.getLogger("app_logger")
-logger.addHandler(loki_handler)
+logger = logging.getLogger("auth_logger")
+
 app = FastAPI(root_path="/playit/auth")
 
 for router in all_routers:
