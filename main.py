1f05ace3921d2140018b1c142336b95b3634f329
Lef <116380867+Stremilov@users.noreply.github.com>
2025-01-10 00:05:12 +0300

    Тестово добавил логи в локи

--- main.py
+++ main.py
@@ -3,17 +3,26 @@ import uvicorn
 import logging
 
 from fastapi import FastAPI
+from logging_loki import LokiHandler
 
 from src.db.db import init_db
 from src.api.routers import all_routers
 
-# Логирую только уровень error+ (error, critical)
+LOKI_URL = "http://loki:3100/loki/api/v1/push"
+LOKI_TAGS = {"application": "playit-auth"}
+
+loki_handler = LokiHandler(
+    url=LOKI_URL,
+    tags=LOKI_TAGS,
+    version="1",
+)
+
 logging.basicConfig(
-    level=logging.ERROR,
+    level=logging.DEBUG,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger("app_logger")
-
+logger.addHandler(loki_handler)
 app = FastAPI(root_path="/playit/auth")
 
 for router in all_routers:
