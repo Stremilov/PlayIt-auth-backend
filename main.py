60c7487ce9dcef8410b0a557a734e27992e78715
cl7paBka <maks-kandikov@yandex.ru>
2025-03-25 20:48:13 +0300

    удалил тот локалхост, потом потру коммиты

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ app.add_middleware(
         "https://it-otdel.space",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "http://172.20.10.9:5173"  # localhost Фронта
     ],
     allow_credentials=True,
     allow_methods=["*"],
